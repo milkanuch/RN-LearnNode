f9730a75e3cd795800a0f6572d3624c534f95448
milkanuch <milkanic2@gmail.com>
2023-05-17 23:55:08 +0300

    feat: add app theme

--- src/navigation/AppNavigation/AppNavigation.tsx
+++ src/navigation/AppNavigation/AppNavigation.tsx
@@ -8,16 +8,16 @@ import {
 import { AppStackNavigation } from 'navigation/AppStackNavigation/AppStackNavigation';
 import { AuthStackNavigation } from 'navigation/AuthStackNavigation/AuthStackNavigation';
 
-import { EDGES } from './appNavigation.settings';
+import { APP_THEME, EDGES } from './appNavigation.settings';
 import { styles } from './appNavigation.styles';
 
 export const AppNavigation = () => {
-  const isUserAuthenticated = true;
+  const isUserAuthenticated = false;
 
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
       <SafeAreaView edges={EDGES} style={styles.screen}>
-        <NavigationContainer>
+        <NavigationContainer theme={APP_THEME}>
           {isUserAuthenticated ? (
             <AppStackNavigation />
           ) : (
--- src/navigation/AppNavigation/appNavigation.settings.ts
+++ src/navigation/AppNavigation/appNavigation.settings.ts
@@ -1,8 +1,17 @@
-import { DefaultTheme } from '@react-navigation/native';
+import { DefaultTheme, Theme } from '@react-navigation/native';
 import { Edge } from 'react-native-safe-area-context';
+
+import { COLORS } from 'constants/colors/colors';
 
 export const EDGES: readonly Edge[] = ['top', 'right', 'left'];
 
-export const APP_THEME = {
+export const APP_THEME: Theme = {
   ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: COLORS.darkGrey,
+    primary: COLORS.purple,
+    text: COLORS.white,
+    card: COLORS.grey,
+  },
 };
