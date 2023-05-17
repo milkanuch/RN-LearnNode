1a13ec84c2a86431d3905a25c81f7752a309e97a
milkanuch <milkanic2@gmail.com>
2023-05-18 00:58:49 +0300

    feat: add title component

---
+++ src/components/Title/title.styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS } from 'constants/colors/colors';
+import { SPACE_MONO_BOLD } from 'constants/fonts/fonts';
+import { SIZES } from 'constants/sizes/sizes';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  title: {
+    color: COLORS.purple,
+    fontSize: 24,
+    marginBottom: SIZES.XS,
+    ...SPACE_MONO_BOLD,
+  },
+});
