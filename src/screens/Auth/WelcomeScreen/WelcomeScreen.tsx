99bf152e38befcccd189081a96b94b8b4abc4977
milkanuch <milkanic2@gmail.com>
2023-05-17 23:55:18 +0300

    feat: add welcome screen

--- src/screens/Auth/WelcomeScreen/WelcomeScreen.tsx
+++ src/screens/Auth/WelcomeScreen/WelcomeScreen.tsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native';
+import { FC } from 'react';
+import { Image, ScrollView, Text, View } from 'react-native';
 
+import { CustomButton } from 'components/CustomButton/CustomButton';
+import { IconSize } from 'components/CustomButton/customButton.types';
+
+import {
+  BUTTON_TITLE,
+  ICON_NAME,
+  SUBTITLE,
+  TITLE,
+  WELCOME_SCREEN_LOGO,
+} from './welcomeScreen.settings';
 import { styles } from './welcomeScreen.styles';
+import {
+  AuthStackNavigationTypes,
+  WelcomeScreenProps,
+} from 'navigation/AuthStackNavigation/authStackNavigation.types';
 
-export const WelcomeScreen = () => {
+export const WelcomeScreen: FC<WelcomeScreenProps> = ({ navigation }) => {
+  const handleContinuePress = () => {
+    navigation.navigate(AuthStackNavigationTypes.SignInScreen);
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text>WelcomeScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={styles.screenContainer}
+      style={styles.screen}>
+      <View>
+        <Image source={WELCOME_SCREEN_LOGO} style={styles.logo} />
+        <Text style={styles.title}>{TITLE}</Text>
+        <Text style={styles.title}>{SUBTITLE}</Text>
+      </View>
+      <CustomButton
+        icon={ICON_NAME}
+        iconSize={IconSize.Medium}
+        onPress={handleContinuePress}
+        style={styles.button}
+        title={BUTTON_TITLE}
+      />
+    </ScrollView>
   );
 };
