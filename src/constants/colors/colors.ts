fe723bed5e3f911e950797f529e008aff1d49ddb
milkanuch <milkanic2@gmail.com>
2023-05-17 23:54:22 +0300

    feat: add colors constants

---
+++ src/constants/colors/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  red: '#FF3742',
+  grey: '#363E40',
+  white: '#ffffff',
+  black: '#000000',
+  green: '#20E070',
+  purple: '#6C63FF',
+  darkGrey: '#363E40',
+  lightGrey: '#949fa6',
+  darkPurple: '#3c30ff',
+  transparent: 'transparent',
+};
