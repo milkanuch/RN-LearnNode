83e356c8b95c771bc5fa0cee0cb9e9de8806f3cb
milkanuch <milkanic2@gmail.com>
2023-05-17 23:53:59 +0300

    feat: add custom fonts

---
+++ src/constants/fonts/fonts.ts
@@ -0,0 +1,22 @@
+import { Font } from './fonts.types';
+
+export const SPACE_MONO_REGULAR: Font = {
+  fontFamily: 'SpaceMono-Regular',
+  fontWeight: '400',
+};
+
+export const SPACE_MONO_REGULAR_ITALIC: Font = {
+  fontFamily: 'SpaceMono-RegularItalic',
+  fontWeight: '400',
+  fontStyle: 'italic',
+};
+
+export const SPACE_MONO_BOLD: Font = {
+  fontFamily: 'SpaceMono-Bold',
+  fontWeight: '700',
+};
+
+export const SPACE_MONO_BOLD_ITALIC: Font = {
+  fontFamily: 'SpaceMono-BoldItalic',
+  fontWeight: '700',
+};
