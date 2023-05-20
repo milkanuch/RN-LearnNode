b617f838c42d4d338176e07537fa7b27c3b3cba9
milkanuch <milkanic2@gmail.com>
2023-05-20 13:01:05 +0300

    feat: add course screen props

--- src/screens/CourseScreen/CourseScreen.tsx
+++ src/screens/CourseScreen/CourseScreen.tsx
@@ -1,8 +1,10 @@
+import { FC } from 'react';
 import { View, Text } from 'react-native';
 
 import { styles } from './courseScreen.styles';
+import { CourseScreenProps } from 'navigation/AppStackNavigation/appStackNavigation.types';
 
-export const CourseScreen = () => {
+export const CourseScreen: FC<CourseScreenProps> = () => {
   return (
     <View style={styles.screen}>
       <Text>CourseScreen</Text>
