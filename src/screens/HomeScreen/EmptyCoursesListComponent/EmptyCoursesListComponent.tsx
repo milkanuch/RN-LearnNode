2e57ff0be0e71a0cf5bd3cd4ff6d659def92cb60
milkanuch <milkanic2@gmail.com>
2023-05-20 13:00:39 +0300

    feat: add empty list component

--- src/screens/HomeScreen/EmptyCoursesListComponent/EmptyCoursesListComponent.tsx
+++ src/screens/HomeScreen/EmptyCoursesListComponent/EmptyCoursesListComponent.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
 import { View, Text } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {
+  NO_COURSES_ICON_COLOR,
+  NO_COURSES_ICON_NAME,
+  NO_COURSES_ICON_SIZE,
+  NO_COURSES_TEXT,
+} from './emptyCoursesListComponent.settings';
+import { styles } from './emptyCoursesListComponent.styles';
 
 export const EmptyCoursesListComponent = () => {
   return (
-    <View>
-      <Text>EmptyCoursesListComponent</Text>
+    <View style={styles.screen}>
+      <Icon
+        color={NO_COURSES_ICON_COLOR}
+        name={NO_COURSES_ICON_NAME}
+        size={NO_COURSES_ICON_SIZE}
+      />
+      <Text style={styles.title}>{NO_COURSES_TEXT}</Text>
     </View>
   );
 };
