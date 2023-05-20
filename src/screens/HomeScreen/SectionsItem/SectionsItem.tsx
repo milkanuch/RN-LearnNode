1b6e670ccffa86c139649b6ec5875aa93eff3489
milkanuch <milkanic2@gmail.com>
2023-05-20 13:00:26 +0300

    feat: add section item component

---
+++ src/screens/HomeScreen/SectionsItem/SectionsItem.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import { View, Text } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+
+import { CustomButton } from 'components/CustomButton/CustomButton';
+import { IconSize } from 'components/CustomButton/customButton.types';
+
+import { Course } from 'services/courses/course.types';
+
+import { BUTTON_TITLE, ICON_NAME } from './sectionsItem.settings';
+import { styles } from './sectionsItem.styles';
+import { AppStackNavigationTypes } from 'navigation/AppStackNavigation/appStackNavigation.types';
+
+export const SectionsItem: FC<Course> = ({ id, name, description }) => {
+  const { navigate } = useNavigation();
+
+  const handleSectionPress = () => {
+    navigate(AppStackNavigationTypes.CourseScreen, { id });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.descriptionContainer}>
+        <Text numberOfLines={1} style={styles.name}>
+          {name}
+        </Text>
+        <Text numberOfLines={4} style={styles.description}>
+          {description}
+        </Text>
+      </View>
+      <CustomButton
+        icon={ICON_NAME}
+        iconSize={IconSize.Large}
+        onPress={handleSectionPress}
+        style={styles.button}
+        title={BUTTON_TITLE}
+      />
+    </View>
+  );
+};
---
+++ src/screens/HomeScreen/SectionsItem/sectionsItem.settings.ts
@@ -0,0 +1,2 @@
+export const BUTTON_TITLE = 'Learn';
+export const ICON_NAME = 'chevron-right';
